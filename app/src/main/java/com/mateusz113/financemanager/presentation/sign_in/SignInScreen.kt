b0e8b7a501c1c2ddf716274821bbed1b5a1603d2
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-28 02:17:51 +0100

    Added SignInScreen
    Temporary screen and viewmodel to test and debug the authorization functionality

---
+++ app/src/main/java/com/mateusz113/financemanager/presentation/sign_in/SignInScreen.kt
@@ -0,0 +1,54 @@
+package com.mateusz113.financemanager.presentation.sign_in
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.mateusz113.financemanager.R
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@RootNavGraph(start = true)
+@Destination
+@Composable
+fun SignInScreen(
+    navigator: DestinationsNavigator,
+    viewModel: SignInViewModel = hiltViewModel(),
+    onSignInClick: () -> Unit
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    LaunchedEffect(key1 = state.signInError) {
+        state.signInError?.let { error ->
+            Toast.makeText(
+                context,
+                error,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = onSignInClick) {
+            Text(text = stringResource(id = R.string.sign_in))
+        }
+    }
+}
