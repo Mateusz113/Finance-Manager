4870d452c04a260276c85d34a065866c081d286d
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-29 20:13:17 +0100

    Updated PaymentListingDto and PaymentDetailsDto
    Updated both dtos to be classes instead of data classes as having no arguments constructor is necessary for firebase api and data classes cannot have one

--- app/src/main/java/com/mateusz113/financemanager/data/repository/dto/PaymentListingDto.kt
+++ app/src/main/java/com/mateusz113/financemanager/data/repository/dto/PaymentListingDto.kt
@@ -1,8 +1,20 @@
 package com.mateusz113.financemanager.data.repository.dto
 
-data class PaymentListingDto (
-    val id: String?,
-    val title: String?,
-    val amount: Float?,
-    val timestamp: Long?
-)
+class PaymentListingDto() {
+    var id: String? = null
+    var title: String? = null
+    var amount: Float? = null
+    var timestamp: Long? = null
+
+    constructor(
+        id: String?,
+        title: String?,
+        amount: Float?,
+        timestamp: Long?
+    ) : this() {
+        this.id = id
+        this.title = title
+        this.amount = amount
+        this.timestamp = timestamp
+    }
+}
