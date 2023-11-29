f0bf45c8107ea832d59440f15c5504191a318685
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-29 14:28:54 +0100

    Update FinanceApplication.kt
    -Assured that Firebase will be caching the data on the device

--- app/src/main/java/com/mateusz113/financemanager/FinanceApplication.kt
+++ app/src/main/java/com/mateusz113/financemanager/FinanceApplication.kt
@@ -1,7 +1,14 @@
 package com.mateusz113.financemanager
 
 import android.app.Application
+import com.google.firebase.database.FirebaseDatabase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FinanceApplication: Application()
+class FinanceApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //Ensure Firebase persistence
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
+    }
+}
