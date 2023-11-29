7f47556bb6302a224f7cd306d9a07f651f1ceade
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-29 15:51:54 +0100

    Update PaymentRepository.kt
    Function addPayment now takes NewPaymentDetails as parameter

--- app/src/main/java/com/mateusz113/financemanager/domain/repository/PaymentRepository.kt
+++ app/src/main/java/com/mateusz113/financemanager/domain/repository/PaymentRepository.kt
@@ -1,5 +1,6 @@
 package com.mateusz113.financemanager.domain.repository
 
+import com.mateusz113.financemanager.domain.model.NewPaymentDetails
 import com.mateusz113.financemanager.domain.model.PaymentDetails
 import com.mateusz113.financemanager.domain.model.PaymentListing
 import com.mateusz113.financemanager.util.Resource
@@ -8,6 +9,6 @@ import kotlinx.coroutines.flow.Flow
 interface PaymentRepository {
     suspend fun getPaymentListings(): Flow<Resource<List<PaymentListing>>>
     suspend fun getPaymentDetails(id: String): Flow<Resource<PaymentDetails>>
-    suspend fun addPayment(payment: PaymentDetails)
+    suspend fun addPayment(payment: NewPaymentDetails)
     suspend fun removePayment(id: String)
 }
