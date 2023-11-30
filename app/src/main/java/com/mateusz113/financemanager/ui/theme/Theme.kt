a23b49348f2fab7edb5ac3bc9e0a3764da65ab57
Mateusz113 <mateusz.ch.113@gmail.com>
2023-11-30 20:27:33 +0100

    Update Theme.kt
    Changed the color of status bar when using the app

--- app/src/main/java/com/mateusz113/financemanager/ui/theme/Theme.kt
+++ app/src/main/java/com/mateusz113/financemanager/ui/theme/Theme.kt
@@ -53,7 +53,7 @@ fun FinanceManagerTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
