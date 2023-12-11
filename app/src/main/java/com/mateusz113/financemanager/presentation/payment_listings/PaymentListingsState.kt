495d717d2eaf0c62ac510d360d0f65b6011a44b5
Mateusz113 <mateusz.ch.113@gmail.com>
2023-12-11 15:43:50 +0100

    Update PaymentListingsState.kt
    Added state of filter dialog and state of filter settings

--- app/src/main/java/com/mateusz113/financemanager/presentation/payment_listings/PaymentListingsState.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/payment_listings/PaymentListingsState.kt
@@ -1,9 +1,12 @@
 package com.mateusz113.financemanager.presentation.payment_listings
 
+import com.mateusz113.financemanager.domain.model.FilterSettings
 import com.mateusz113.financemanager.domain.model.PaymentListing
 
 data class PaymentListingsState(
     val payments: List<PaymentListing> = emptyList(),
     val isLoading: Boolean = false,
-    val isRefreshing: Boolean = false
+    val isRefreshing: Boolean = false,
+    val isFilterDialogOpen: Boolean = false,
+    val filterSettings: FilterSettings = FilterSettings()
 )
