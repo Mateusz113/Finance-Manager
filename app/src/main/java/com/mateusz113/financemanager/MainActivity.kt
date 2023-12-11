3e9ca4647306817c2c049473a2191a5af39e3192
Mateusz113 <mateusz.ch.113@gmail.com>
2023-12-11 15:49:20 +0100

    Update MainActivity.kt
    -Added the way to distinguish if user is authenticated or not at the app startup and skip the navigation to sign in screen if needed
    -Added navController to DestinationsNavHost and BottomNavigationBar to correctly change between screens
    -Wrapped the application in scaffold that contains the BottomNavigationBar

--- app/src/main/java/com/mateusz113/financemanager/MainActivity.kt
+++ app/src/main/java/com/mateusz113/financemanager/MainActivity.kt
@@ -7,23 +7,29 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.rememberNavController
 import com.google.android.gms.auth.api.identity.Identity
 import com.mateusz113.financemanager.presentation.NavGraphs
+import com.mateusz113.financemanager.presentation.appCurrentDestinationAsState
 import com.mateusz113.financemanager.presentation.destinations.ProfileScreenDestination
 import com.mateusz113.financemanager.presentation.destinations.SignInScreenDestination
 import com.mateusz113.financemanager.presentation.auth.GoogleAuthUiClient
+import com.mateusz113.financemanager.presentation.common.BottomNavigationBar
+import com.mateusz113.financemanager.presentation.destinations.Destination
+import com.mateusz113.financemanager.presentation.destinations.PaymentDetailsScreenDestination
 import com.mateusz113.financemanager.presentation.destinations.PaymentListingsScreenDestination
 import com.mateusz113.financemanager.presentation.sign_in.SignInScreen
 import com.mateusz113.financemanager.presentation.sign_in.SignInViewModel
@@ -31,6 +37,7 @@ import com.mateusz113.financemanager.presentation.profile.ProfileScreen
 import com.mateusz113.financemanager.ui.theme.FinanceManagerTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.manualcomposablecalls.composable
+import com.ramcosta.composedestinations.navigation.popUpTo
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -47,102 +54,124 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FinanceManagerTheme {
+                val screensWithoutBottomNav = remember {
+                    mutableListOf<Destination>(
+                        PaymentDetailsScreenDestination,
+                        SignInScreenDestination
+                    )
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    DestinationsNavHost(navGraph = NavGraphs.root) {
-                        composable(SignInScreenDestination) {
-                            val viewModel = hiltViewModel<SignInViewModel>()
-                            val state by viewModel.state.collectAsStateWithLifecycle()
+                    val navController = rememberNavController()
+                    val currentDestination = navController.appCurrentDestinationAsState().value
+                    val startRoute =
+                        if (googleAuthUiClient.getSignedInUser() == null) SignInScreenDestination else NavGraphs.root.startRoute
 
-                            //Navigates to signed in screen if sign in was successful
-                            LaunchedEffect(key1 = Unit){
-                                if (googleAuthUiClient.getSignedInUser() != null){
-                                    destinationsNavigator.navigate(PaymentListingsScreenDestination)
-                                }
+                    Scaffold(
+                        bottomBar = {
+                            if (!screensWithoutBottomNav.contains(currentDestination)) {
+                                BottomNavigationBar(
+                                    navController = navController,
+                                    startRoute = startRoute
+                                )
                             }
+                        }
+                    ) {
+                        Column(modifier = Modifier.padding(it)) {
+                            DestinationsNavHost(
+                                navGraph = NavGraphs.root,
+                                navController = navController,
+                                startRoute = startRoute
+                            ) {
+                                composable(SignInScreenDestination) {
+                                    val viewModel = hiltViewModel<SignInViewModel>()
+                                    val state by viewModel.state.collectAsStateWithLifecycle()
 
-                            //Intent launcher
-                            val launcher = rememberLauncherForActivityResult(
-                                contract = ActivityResultContracts.StartIntentSenderForResult(),
-                                onResult = { result ->
-                                    if (result.resultCode == RESULT_OK) {
-                                        lifecycleScope.launch {
-                                            val signInResult = googleAuthUiClient.signInWithIntent(
-                                                intent = result.data ?: return@launch
-                                            )
-                                            viewModel.onSignInResult(signInResult)
+                                    //Navigates to signed in screen if sign in was successful
+                                    LaunchedEffect(key1 = Unit) {
+                                        if (googleAuthUiClient.getSignedInUser() != null) {
+                                            destinationsNavigator.navigate(
+                                                PaymentListingsScreenDestination
+                                            ) {
+                                                popUpTo(NavGraphs.root.startRoute) {
+                                                    inclusive = true
+                                                }
+                                            }
                                         }
                                     }
+
+                                    //Intent launcher
+                                    val launcher = rememberLauncherForActivityResult(
+                                        contract = ActivityResultContracts.StartIntentSenderForResult(),
+                                        onResult = { result ->
+                                            if (result.resultCode == RESULT_OK) {
+                                                lifecycleScope.launch {
+                                                    val signInResult =
+                                                        googleAuthUiClient.signInWithIntent(
+                                                            intent = result.data ?: return@launch
+                                                        )
+                                                    viewModel.onSignInResult(signInResult)
+                                                }
+                                            }
+                                        }
+                                    )
+
+                                    //Navigates user to signed it page when the sign in is successful
+                                    LaunchedEffect(
+                                        key1 = state.isSignInSuccessful,
+                                    ) {
+                                        if (state.isSignInSuccessful) {
+                                            Toast.makeText(
+                                                applicationContext,
+                                                "Sign in successful",
+                                                Toast.LENGTH_LONG
+                                            ).show()
+                                            destinationsNavigator.navigate(
+                                                PaymentListingsScreenDestination
+                                            )
+                                            viewModel.resetState()
+
+                                        }
+                                    }
+
+                                    SignInScreen(
+                                        navigator = destinationsNavigator,
+                                        onSignInClick = {
+                                            lifecycleScope.launch {
+                                                val signInIntentSender =
+                                                    googleAuthUiClient.getIntentSender()
+                                                launcher.launch(
+                                                    IntentSenderRequest.Builder(
+                                                        signInIntentSender ?: return@launch
+                                                    ).build()
+                                                )
+                                            }
+                                        }
+                                    )
                                 }
-                            )
-
-                            //Navigates user to signed it page when the sign in is successful
-                            LaunchedEffect(
-                                key1 = state.isSignInSuccessful,
-                            ) {
-                                if (state.isSignInSuccessful) {
-                                    Toast.makeText(
-                                        applicationContext,
-                                        "Sign in successful",
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                    destinationsNavigator.navigate(PaymentListingsScreenDestination)
-                                    viewModel.resetState()
-
+                                composable(ProfileScreenDestination) {
+                                    ProfileScreen(
+                                        userData = googleAuthUiClient.getSignedInUser(),
+                                        onSignOutClick = {
+                                            lifecycleScope.launch {
+                                                googleAuthUiClient.signOut()
+                                                Toast.makeText(
+                                                    applicationContext,
+                                                    "Signed out",
+                                                    Toast.LENGTH_LONG
+                                                ).show()
+                                                destinationsNavigator.navigate(SignInScreenDestination)
+                                            }
+                                        })
                                 }
                             }
-
-                            SignInScreen(
-                                navigator = destinationsNavigator,
-                                onSignInClick = {
-                                    lifecycleScope.launch {
-                                        val signInIntentSender = googleAuthUiClient.getIntentSender()
-                                        launcher.launch(
-                                            IntentSenderRequest.Builder(
-                                                signInIntentSender ?: return@launch
-                                            ).build()
-                                        )
-                                    }
-                                }
-                            )
-                        }
-                        composable(ProfileScreenDestination) {
-                            ProfileScreen(
-                                userData = googleAuthUiClient.getSignedInUser(),
-                                onSignOutClick = {
-                                    lifecycleScope.launch {
-                                        googleAuthUiClient.signOut()
-                                        Toast.makeText(
-                                            applicationContext,
-                                            "Signed out",
-                                            Toast.LENGTH_LONG
-                                        ).show()
-                                        destinationsNavigator.popBackStack()
-                                    }
-                                })
                         }
                     }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinanceManagerTheme {
-        Greeting("Android")
     }
 }
