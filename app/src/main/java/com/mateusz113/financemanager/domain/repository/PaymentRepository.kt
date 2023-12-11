7b6af6c103011064b777886667cea9db5ecf9b28
Mateusz113 <mateusz.ch.113@gmail.com>
2023-12-11 14:54:45 +0100

    Update PaymentRepository.kt
    Added a function to get payment listings with specific filter

--- app/src/main/java/com/mateusz113/financemanager/domain/repository/PaymentRepository.kt
+++ app/src/main/java/com/mateusz113/financemanager/domain/repository/PaymentRepository.kt
@@ -1,5 +1,6 @@
 package com.mateusz113.financemanager.domain.repository
 
+import com.mateusz113.financemanager.domain.model.FilterSettings
 import com.mateusz113.financemanager.domain.model.NewPaymentDetails
 import com.mateusz113.financemanager.domain.model.PaymentDetails
 import com.mateusz113.financemanager.domain.model.PaymentListing
@@ -8,6 +9,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface PaymentRepository {
     suspend fun getPaymentListings(): Flow<Resource<List<PaymentListing>>>
+    suspend fun getPaymentListingsWithFilter(filterSettings: FilterSettings): Flow<Resource<List<PaymentListing>>>
     suspend fun getPaymentDetails(id: String): Flow<Resource<PaymentDetails>>
     suspend fun addPayment(payment: NewPaymentDetails)
     suspend fun removePayment(id: String)
