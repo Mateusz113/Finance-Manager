cd5d6c90a664e8af836bb1997efaae21e50fe6c4
Mateusz113 <mateusz.ch.113@gmail.com>
2023-12-11 14:59:16 +0100

    Create FilterSettings.kt
    Added a filter used in searching for specific listings

---
+++ app/src/main/java/com/mateusz113/financemanager/domain/model/FilterSettings.kt
@@ -0,0 +1,14 @@
+package com.mateusz113.financemanager.domain.model
+
+import com.mateusz113.financemanager.util.Category
+import java.time.LocalDate
+import java.time.Month
+
+data class FilterSettings(
+    val query: String = "",
+    val categories: MutableList<Category?> = mutableListOf(),
+    val minValue: String = "",
+    val maxValue: String = "",
+    val startDate: LocalDate = LocalDate.of(2011, Month.JULY, 22),
+    val endDate: LocalDate = LocalDate.now()
+)
