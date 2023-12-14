b0c7b7130ca0386791d45993a440f4933e8b76b6
Mateusz113 <mateusz.ch.113@gmail.com>
2023-12-14 21:54:35 +0100

    Create TopAppBarWithBack.kt
    Added a component used as a top bar in screens without bottom navigation

---
+++ app/src/main/java/com/mateusz113/financemanager/presentation/common/TopAppBarWithBack.kt
@@ -0,0 +1,53 @@
+package com.mateusz113.financemanager.presentation.common
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.mateusz113.financemanager.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarWithBack(
+    @StringRes label: Int,
+    navController: NavController
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = label),
+                textAlign = TextAlign.Center
+            )
+        },
+        navigationIcon = {
+            Box(
+                modifier = Modifier
+                    .clickable {
+                        navController.popBackStack()
+                    }
+                    .size(48.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(id = R.string.go_back),
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+        }
+    )
+}
