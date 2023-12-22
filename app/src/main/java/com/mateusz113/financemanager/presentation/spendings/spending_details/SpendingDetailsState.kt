65445139ed3755687533735ad81dc246cb4a1f1a
Mateusz113 <mateusz.ch.113@gmail.com>
2023-12-22 22:18:19 +0100

    Update SpendingDetailsState.kt
    Added necessary states to show dialogs and added the map to easily access the listings for each category

--- app/src/main/java/com/mateusz113/financemanager/presentation/spendings/spending_details/SpendingDetailsState.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/spendings/spending_details/SpendingDetailsState.kt
@@ -1,12 +1,22 @@
 package com.mateusz113.financemanager.presentation.spendings.spending_details
 
+import androidx.compose.ui.graphics.Color
+import co.yml.charts.ui.piechart.models.PieChartData
+import com.mateusz113.financemanager.domain.model.Category
 import com.mateusz113.financemanager.domain.model.FilterSettings
 import com.mateusz113.financemanager.domain.model.PaymentListing
 
 data class SpendingDetailsState(
     val paymentListings: List<PaymentListing> = emptyList(),
+    val listingsMap: Map<Category, List<PaymentListing>> = emptyMap(),
     val filterSettings: FilterSettings = FilterSettings(),
     val isLoading: Boolean = false,
-    val isDialogOpen: Boolean = false,
+    val isFilterDialogOpen: Boolean = false,
+    val isKeyDialogOpen: Boolean = false,
+    val currentSlice: PieChartData.Slice = PieChartData.Slice(
+        label = Category.Housing.name,
+        value = 0f,
+        color = Color.Black
+    ),
     val error: String? = null
 )
