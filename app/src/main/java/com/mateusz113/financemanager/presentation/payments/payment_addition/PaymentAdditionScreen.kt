d34714db9214c259e3a5fd4353c8ca32fdb57924
Mateusz113 <mateusz.ch.113@gmail.com>
2023-12-29 22:29:32 +0100

    Update PaymentAdditionScreen.kt
    Changed the snackbar for better user experience

--- app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_addition/PaymentAdditionScreen.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_addition/PaymentAdditionScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -95,7 +96,12 @@ fun PaymentAdditionScreen(
                 )
             },
             snackbarContent = { snackbarData ->
-                Snackbar(snackbarData = snackbarData)
+                Snackbar(
+                    modifier = Modifier
+                        .offset(y = (-60).dp),
+                    containerColor = MaterialTheme.colorScheme.inverseSurface,
+                    snackbarData = snackbarData
+                )
             },
             snackbarHostState = snackbarHostState
         ) { innerPadding ->
