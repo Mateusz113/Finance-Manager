7d164ba7dfddefa93024c8bfee14fc01a9563f97
Mateusz113 <mateusz.ch.113@gmail.com>
2024-01-09 20:10:46 +0100

    Updated PaymentDetails elements
    Added the necessary elements to retrieve and show the options implemented in the settings

--- app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_details/PaymentDetailsState.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_details/PaymentDetailsState.kt
@@ -1,9 +1,12 @@
 package com.mateusz113.financemanager.presentation.payments.payment_details
 
 import com.mateusz113.financemanager.domain.model.PaymentDetails
+import com.mateusz113.financemanager.util.Currency
 
 data class PaymentDetailsState<T>(
     var paymentDetails: PaymentDetails? = null,
+    val currency: Currency = Currency.PLN,
+    val isCurrencyPrefix: Boolean? = null,
     var error: String? = null,
     var isLoading: Boolean = false,
     var isRefreshing: Boolean = false,
