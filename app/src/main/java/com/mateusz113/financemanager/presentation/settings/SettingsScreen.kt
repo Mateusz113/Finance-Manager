2e23dc77548d093eefc9b868cc126146bca91b01
Mateusz113 <mateusz.ch.113@gmail.com>
2024-01-09 20:09:42 +0100

    Update SettingsScreen.kt
    Added the currently implemented options representations and visual way to change them

--- app/src/main/java/com/mateusz113/financemanager/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/settings/SettingsScreen.kt
@@ -11,14 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.mateusz113.financemanager.R
 import com.mateusz113.financemanager.presentation.common.components.TopAppBarWithBack
+import com.mateusz113.financemanager.presentation.common.dialog.RadioButtonSelectionDialog
 import com.mateusz113.financemanager.presentation.common.wrapper.ScaffoldWrapper
 import com.mateusz113.financemanager.presentation.settings.components.SettingsItem
+import com.mateusz113.financemanager.util.Currency
+import com.mateusz113.financemanager.util.SymbolPlacement
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 
@@ -50,16 +54,52 @@ fun SettingsScreen(
         ) {
             SettingsItem(
                 modifier = itemModifier,
-                optionLabel = "Label",
-                currentSetting = "Option",
-                onClick = {}
+                optionLabel = stringResource(id = R.string.currency_option),
+                currentSetting = stringResource(
+                    id = Currency.labelMap[state.currentCurrency] ?: R.string.unknown
+                ),
+                onClick = {
+                    viewModel.onEvent(
+                        SettingsEvent.UpdateDialogInfo(
+                            label = R.string.currency,
+                            currentOption = state.currentCurrency,
+                            optionsLabelsMap = Currency.labelMap,
+                            optionsList = Currency.values().toList()
+                        )
+                    )
+                    viewModel.onEvent(SettingsEvent.UpdateDialogState(true))
+                }
             )
             SettingsItem(
                 modifier = itemModifier,
-                optionLabel = "Label",
-                currentSetting = "Option",
-                onClick = {}
+                optionLabel = stringResource(id = R.string.symbol_placement_option),
+                currentSetting = stringResource(
+                    id = SymbolPlacement.labelMap[state.currentSymbolPlacement] ?: R.string.unknown
+                ),
+                onClick = {
+                    viewModel.onEvent(
+                        SettingsEvent.UpdateDialogInfo(
+                            label = R.string.symbol_placement,
+                            currentOption = state.currentSymbolPlacement,
+                            optionsLabelsMap = SymbolPlacement.labelMap,
+                            optionsList = SymbolPlacement.values().toList()
+                        )
+                    )
+                    viewModel.onEvent(SettingsEvent.UpdateDialogState(true))
+                }
             )
         }
+
+        RadioButtonSelectionDialog(
+            isDialogOpen = state.isDialogOpen,
+            dialogInfo = state.dialogInfo,
+            onDismiss = {
+                viewModel.onEvent(SettingsEvent.UpdateDialogState(false))
+            },
+            onOptionSelect = { option ->
+                viewModel.onEvent(SettingsEvent.UpdateDialogState(false))
+                viewModel.onEvent(SettingsEvent.UpdateSelectedOption(option))
+            }
+        )
     }
 }
