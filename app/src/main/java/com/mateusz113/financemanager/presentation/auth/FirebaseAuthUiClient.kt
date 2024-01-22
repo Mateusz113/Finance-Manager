080b3491792046777791f9b318e8e00a313faf0a
Mateusz113 <mateusz.ch.113@gmail.com>
2024-01-22 17:30:55 +0100

    Added classes templates for missing auithentication methods

---
+++ app/src/main/java/com/mateusz113/financemanager/presentation/auth/FirebaseAuthUiClient.kt
@@ -0,0 +1,7 @@
+package com.mateusz113.financemanager.presentation.auth
+
+class FirebaseAuthUiClient() : AuthUiClient {
+    override suspend fun signOut() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/mateusz113/financemanager/presentation/auth/GitHubAuthUiClient.kt
@@ -0,0 +1,7 @@
+package com.mateusz113.financemanager.presentation.auth
+
+class GitHubAuthUiClient() : AuthUiClient {
+    override suspend fun signOut() {
+        TODO("Not yet implemented")
+    }
+}
