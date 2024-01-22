791befbf684825d0f9206ef68c061a7d6e7fb283
Mateusz113 <mateusz.ch.113@gmail.com>
2024-01-22 17:34:22 +0100

    Update ProfileViewModel.kt
    -Replaced the googleAuthUiClient reference with interface reference
    -Moved the way that the current used data is retrieved to the firebaseAuth

--- app/src/main/java/com/mateusz113/financemanager/presentation/profile/ProfileViewModel.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/profile/ProfileViewModel.kt
@@ -1,16 +1,11 @@
 package com.mateusz113.financemanager.presentation.profile
 
-import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
-import android.widget.Toast
-import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
 import com.mateusz113.financemanager.domain.repository.PaymentRepository
-import com.mateusz113.financemanager.presentation.auth.GoogleAuthUiClient
+import com.mateusz113.financemanager.presentation.auth.AuthUiClient
 import com.mateusz113.financemanager.util.Resource
 import com.mateusz113.financemanager.util.convertTimestampIntoLocalDate
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -64,11 +59,11 @@ class ProfileViewModel @Inject constructor(
     }
 
     fun updateAuthClient(
-        googleAuthUiClient: GoogleAuthUiClient
+        authUiClient: AuthUiClient
     ) {
-        if (_state.value.googleAuthUiClient == null) {
+        if (_state.value.authUiClient == null) {
             _state.value = _state.value.copy(
-                googleAuthUiClient = googleAuthUiClient
+                authUiClient = authUiClient
             )
             //After the auth client is inserted the user info can be updated
             updateProfileInfo()
@@ -84,8 +79,8 @@ class ProfileViewModel @Inject constructor(
     private fun updateJoinDate() {
         val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
         val user = FirebaseAuth.getInstance().currentUser
-        _state.value.googleAuthUiClient?.getSignedInUser()?.let { userData ->
-            if (!sharedPreferences.contains("${userData.userId}JoinDate")) {
+        user.let { userData ->
+            if (!sharedPreferences.contains("${userData?.uid}JoinDate")) {
                 val userId = user?.uid ?: ""
                 val userJoinTimestamp = user?.metadata?.creationTimestamp
                 if (userJoinTimestamp != null) {
@@ -103,7 +98,7 @@ class ProfileViewModel @Inject constructor(
             }
             val joinDate =
                 sharedPreferences.getString(
-                    "${userData.userId}JoinDate",
+                    "${userData?.uid}JoinDate",
                     formatter.format(LocalDate.now())
                 )
             _state.value = _state.value.copy(
@@ -113,11 +108,11 @@ class ProfileViewModel @Inject constructor(
     }
 
     private fun updatePaymentNumber() {
-        _state.value.googleAuthUiClient?.getSignedInUser()?.let { userData ->
-            if (!sharedPreferences.contains("${userData.userId}PaymentsNum")) {
-                updateNumOfPaymentsInSharedPrefs(userData.userId)
+        FirebaseAuth.getInstance().currentUser?.let { userData ->
+            if (!sharedPreferences.contains("${userData.uid}PaymentsNum")) {
+                updateNumOfPaymentsInSharedPrefs(userData.uid)
             }
-            val paymentsNumber = sharedPreferences.getInt("${userData.userId}PaymentsNum", 0)
+            val paymentsNumber = sharedPreferences.getInt("${userData.uid}PaymentsNum", 0)
             _state.value = _state.value.copy(
                 paymentsNumber = paymentsNumber
             )
@@ -126,12 +121,12 @@ class ProfileViewModel @Inject constructor(
 
 
     private fun updateUserInfo() {
-        _state.value.googleAuthUiClient?.getSignedInUser()?.let { userData ->
+        FirebaseAuth.getInstance().currentUser?.let { userData ->
             _state.value = _state.value.copy(
-                userId = userData.userId,
-                username = userData.username,
+                userId = userData.uid,
+                username = userData.displayName,
                 email = userData.email,
-                profilePictureUrl = userData.profilePictureUrl
+                profilePictureUrl = userData.photoUrl?.toString()
             )
         }
     }
@@ -166,7 +161,7 @@ class ProfileViewModel @Inject constructor(
     }
 
     suspend fun signOut() {
-        _state.value.googleAuthUiClient?.signOut()
+        _state.value.authUiClient?.signOut()
     }
 
     suspend fun deleteAccount(): Boolean = suspendCoroutine { continuation ->
