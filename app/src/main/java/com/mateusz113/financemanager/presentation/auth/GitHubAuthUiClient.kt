82689af9d605e0e3e9516e3a9a8f8d73c6c7d8cc
Mateusz113 <mateusz.ch.113@gmail.com>
2024-01-27 00:05:28 +0100

    Update GitHubAuthUiClient.kt
    -Added a functionality implementation

--- app/src/main/java/com/mateusz113/financemanager/presentation/auth/GitHubAuthUiClient.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/auth/GitHubAuthUiClient.kt
@@ -1,7 +1,79 @@
 package com.mateusz113.financemanager.presentation.auth
 
-class GitHubAuthUiClient() : AuthUiClient {
+import android.app.Activity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class GitHubAuthUiClient(
+    private val firebaseAuth: FirebaseAuth,
+    private val activity: Activity
+) : AuthUiClient {
+    private val tag = "GITHUB_INFO"
+    fun signIn(
+        onSignInComplete: (SignInResult) -> Unit
+    ) {
+        val pendingSignInTask = firebaseAuth.pendingAuthResult
+        if (pendingSignInTask != null) {
+            pendingSignInTask
+                .addOnSuccessListener {
+                    Log.d(tag, "Pending sign in task successfully completed")
+                    if (it.user != null) {
+                        onSignInComplete(
+                            SignInResult(
+                                wasSignInSuccessful = true,
+                                errorMessage = null
+                            )
+                        )
+                    }
+                }
+                .addOnFailureListener {
+                    Log.d(tag, "Logging failed: ${it.message}")
+                    onSignInComplete(
+                        SignInResult(
+                            wasSignInSuccessful = false,
+                            errorMessage = it.message
+                        )
+                    )
+                }
+        } else {
+            firebaseAuth
+                .startActivityForSignInWithProvider(activity, buildProvider())
+                .addOnSuccessListener {
+                    Log.d(tag, "Logging success")
+                    if (it.user != null) {
+                        onSignInComplete(
+                            SignInResult(
+                                wasSignInSuccessful = true,
+                                errorMessage = null
+                            )
+                        )
+                    }
+                }
+                .addOnFailureListener {
+                    Log.d(tag, "Logging failure: ${it.message}")
+                    onSignInComplete(
+                        SignInResult(
+                            wasSignInSuccessful = false,
+                            errorMessage = it.message
+                        )
+                    )
+                }
+        }
+    }
+
+    private fun buildProvider(): OAuthProvider {
+        val provider = OAuthProvider.newBuilder("github.com")
+        provider.addCustomParameter("login", "your-email.com")
+        return provider.build()
+    }
+
     override suspend fun signOut() {
-        TODO("Not yet implemented")
+        Firebase.auth.signOut()
     }
 }
