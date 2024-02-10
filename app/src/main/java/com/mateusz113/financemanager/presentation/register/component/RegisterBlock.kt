2311739a94d030f88ace13d80edc42f9fa2913d3
Mateusz113 <mateusz.ch.113@gmail.com>
2024-02-10 19:37:44 +0100

    Update RegisterBlock.kt
    -Extracted texts into strings.xml
    -Changed button design to match the one in SignInScreen
    -Changes made based on introducing AccountInfoValidator

--- app/src/main/java/com/mateusz113/financemanager/presentation/register/component/RegisterBlock.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/register/component/RegisterBlock.kt
@@ -3,8 +3,10 @@ package com.mateusz113.financemanager.presentation.register.component
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,11 +16,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.mateusz113.financemanager.R
 import com.mateusz113.financemanager.presentation.common.components.InputTextField
+import com.mateusz113.financemanager.util.AccountInfoValidator
 
 @Composable
 fun RegisterBlock(
+    modifier: Modifier,
     onRegisterClick: (String, String, String) -> Unit
 ) {
     val context = LocalContext.current
@@ -26,109 +34,103 @@ fun RegisterBlock(
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
+    val buttonTextStyle = TextStyle(
+        fontSize = MaterialTheme.typography.titleMedium.fontSize,
+        fontWeight = FontWeight.Medium
+    )
+
     Column(
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         InputTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = displayName,
             onValueChange = {
                 displayName = it
             },
             label = {
-                Text(text = "Username")
+                Text(text = stringResource(id = R.string.username))
             },
-            isError = !isDisplayNameValid(displayName) && displayName.isNotEmpty(),
+            isError = !AccountInfoValidator.displayNameValidator(displayName) && displayName.isNotEmpty(),
             isPassword = false,
         )
         Spacer(modifier = Modifier.height(10.dp))
 
         InputTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = email,
             onValueChange = {
                 email = it
             },
             label = {
-                Text(text = "Email")
+                Text(text = stringResource(id = R.string.email))
             },
-            isError = !isEmailValid(email) && email.isNotEmpty(),
+            isError = !AccountInfoValidator.emailValidator(email) && email.isNotEmpty(),
             isPassword = false
         )
         Spacer(modifier = Modifier.height(10.dp))
 
         InputTextField(
+            modifier = Modifier.fillMaxWidth(),
             value = password,
             onValueChange = {
                 password = it
             },
             label = {
-                Text(text = "Password")
+                Text(text = stringResource(id = R.string.password))
             },
-            isError = !isPasswordValid(password) && password.isNotEmpty(),
+            isError = !AccountInfoValidator.passwordValidator(password) && password.isNotEmpty(),
             isPassword = true
         )
         Spacer(modifier = Modifier.height(20.dp))
 
-        Button(onClick = {
-            when (true) {
-                !isDisplayNameValid(displayName) -> {
-                    Toast.makeText(
-                        context,
-                        "Display name is too long or empty. It must be at most 50 characters long.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+        Button(
+            modifier = Modifier.fillMaxWidth(0.5f),
+            onClick = {
+                when (true) {
+                    !AccountInfoValidator.displayNameValidator(displayName) -> {
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.display_name_error),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
-                !isEmailValid(email) -> {
-                    Toast.makeText(
-                        context,
-                        "Email is blank or incorrectly formatted.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                    !AccountInfoValidator.emailValidator(email) -> {
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.email_error),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
-                !isPasswordValid(password) -> {
-                    Toast.makeText(
-                        context,
-                        "Password is too short. It must be at least 6 characters long.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                    !AccountInfoValidator.passwordValidator(password) -> {
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.password_error),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
-                true -> {
-                    onRegisterClick(displayName, email, password)
-                }
+                    true -> {
+                        onRegisterClick(displayName, email, password)
+                    }
 
-                else -> {
-                    Toast.makeText(
-                        context,
-                        "Error occurred",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    else -> {
+                        Toast.makeText(
+                            context,
+                            context.getString(R.string.generic_error),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
             }
-        }) {
-            Text(text = "Register")
+        ) {
+            Text(
+                text = stringResource(id = R.string.register),
+                style = buttonTextStyle
+            )
         }
     }
-}
-
-private fun isDisplayNameValid(
-    displayName: String
-): Boolean {
-    if (displayName.isEmpty()){
-        return false
-    }
-    return displayName.length < 50
-}
-
-private fun isEmailValid(email: String): Boolean {
-    if (email.isEmpty()){
-        return false
-    }
-    val emailRegex = Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
-    return email.matches(emailRegex)
-}
-
-private fun isPasswordValid(password: String): Boolean {
-    return password.length >= 6
 }
