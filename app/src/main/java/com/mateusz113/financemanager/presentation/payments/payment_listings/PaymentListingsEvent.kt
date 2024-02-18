e220c606513760307eaaf24a7250e9f777cc3e4d
Mateusz113 <mateusz.ch.113@gmail.com>
2024-02-18 16:55:24 +0100

    Update PaymentListingsEvent.kt
    Added events to manipulate sorting settings dialog state and updating the sorting method

--- app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_listings/PaymentListingsEvent.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_listings/PaymentListingsEvent.kt
@@ -1,6 +1,7 @@
 package com.mateusz113.financemanager.presentation.payments.payment_listings
 
 import com.mateusz113.financemanager.domain.model.FilterSettings
+import com.mateusz113.financemanager.util.SortingMethod
 
 sealed class PaymentListingsEvent {
     object Refresh : PaymentListingsEvent()
@@ -8,6 +9,8 @@ sealed class PaymentListingsEvent {
     data class UpdateFilterSettings(val filterSettings: FilterSettings) : PaymentListingsEvent()
     data class UpdateDeleteDialogState(val isOpen: Boolean) : PaymentListingsEvent()
     data class UpdateDeleteDialogInfo(val title: String, val id: String) : PaymentListingsEvent()
+    data class UpdateSortingMethod(val sortingMethod: SortingMethod): PaymentListingsEvent()
+    data class UpdateSortingDialogState(val isOpen: Boolean): PaymentListingsEvent()
     data class DeletePayment(val id: String) : PaymentListingsEvent()
     data class SearchPayment(val query: String) : PaymentListingsEvent()
 
