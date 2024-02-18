6dd94f92dad175534041f44d287418f8410f83ff
Mateusz113 <mateusz.ch.113@gmail.com>
2024-02-18 17:00:17 +0100

    Update PaymentListingsViewModel.kt
    Removed unnecessary import and added end of file newline

--- app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_listings/PaymentListingsViewModel.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_listings/PaymentListingsViewModel.kt
@@ -1,7 +1,6 @@
 package com.mateusz113.financemanager.presentation.payments.payment_listings
 
 import android.content.SharedPreferences
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
