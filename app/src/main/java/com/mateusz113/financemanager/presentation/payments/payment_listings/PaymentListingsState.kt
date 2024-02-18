7f7c36fa0dcd58be45e4cb7d4160466261921571
Mateusz113 <mateusz.ch.113@gmail.com>
2024-02-18 16:57:44 +0100

    Update PaymentListingsState.kt
    Added states for:
    -information whether the dialog for changing the sorting method is open
    -information passed down to dialog about current selection and options

--- app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_listings/PaymentListingsState.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_listings/PaymentListingsState.kt
@@ -1,8 +1,11 @@
 package com.mateusz113.financemanager.presentation.payments.payment_listings
 
+import com.mateusz113.financemanager.R
 import com.mateusz113.financemanager.domain.model.FilterSettings
 import com.mateusz113.financemanager.domain.model.PaymentListing
+import com.mateusz113.financemanager.presentation.common.dialog.RadioButtonsDialogInfo
 import com.mateusz113.financemanager.util.Currency
+import com.mateusz113.financemanager.util.SortingMethod
 
 data class PaymentListingsState(
     val payments: List<PaymentListing> = emptyList(),
@@ -14,5 +17,12 @@ data class PaymentListingsState(
     val filterSettings: FilterSettings = FilterSettings(),
     val isDeleteDialogOpen: Boolean = false,
     val deleteDialogPaymentTitle: String = "",
-    val deleteDialogPaymentId: String = ""
+    val deleteDialogPaymentId: String = "",
+    val isSortingMethodDialogOpen: Boolean = false,
+    val sortingSettingsInfo: RadioButtonsDialogInfo<SortingMethod> = RadioButtonsDialogInfo(
+        label = R.string.sorting_method,
+        currentOption = SortingMethod.Alphabetically,
+        optionsLabelsMap = SortingMethod.labelMap,
+        optionsList = SortingMethod.values().toList()
+    )
 )
