8a3df3522467a4583847bd097864d0c13ce5ab16
Mateusz113 <mateusz.ch.113@gmail.com>
2024-03-09 16:38:04 +0100

    Update TestTags.kt
    Added test tag for photos

--- app/src/main/java/com/mateusz113/financemanager/util/TestTags.kt
+++ app/src/main/java/com/mateusz113/financemanager/util/TestTags.kt
@@ -1,6 +1,7 @@
 package com.mateusz113.financemanager.util
 
 object TestTags {
+    const val PHOTO = "PHOTO"
     const val LAZY_COLUMN = "LAZY_COLUMN"
     const val DROPDOWN_MENU = "DROPDOWN_MENU"
 }
