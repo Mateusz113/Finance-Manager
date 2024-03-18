e3e18b1305466a0acf6aa82d504c475fd3f3c517
Mateusz113 <mateusz.ch.113@gmail.com>
2024-03-18 17:26:12 +0100

    Update build.gradle.kts
    Added new dependency to allow access to official pull referesh functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation(platform("androidx.compose:compose-bom:2024.02.01"))
+    implementation("androidx.compose.material:material")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
