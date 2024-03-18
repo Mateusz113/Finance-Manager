986dceee880297dba1ceae8ced29aaa66483c9dd
Mateusz113 <mateusz.ch.113@gmail.com>
2024-03-18 18:37:53 +0100

    Update TestTags.kt
    Added more test tags

--- app/src/main/java/com/mateusz113/financemanager/util/TestTags.kt
+++ app/src/main/java/com/mateusz113/financemanager/util/TestTags.kt
@@ -1,7 +1,10 @@
 package com.mateusz113.financemanager.util
 
 object TestTags {
+    const val SWIPE_REFRESH_INDICATOR = "SWIPE_REFRESH_INDICATOR"
     const val PHOTO = "PHOTO"
+    const val PHOTO_WRAPPER = "PHOTO_WRAPPER"
     const val LAZY_COLUMN = "LAZY_COLUMN"
     const val DROPDOWN_MENU = "DROPDOWN_MENU"
+    const val SWIPE_REFRESH = "SWIPE_REFRESH"
 }
