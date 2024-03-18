e27effa94a6786c6c09608acd53fe280b56f7e72
Mateusz113 <mateusz.ch.113@gmail.com>
2024-03-18 17:27:34 +0100

    Update PaymentDetailsScreen.kt
    Replaced SwipeRefresh with pullRefersh

--- app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_details/PaymentDetailsScreen.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/payments/payment_details/PaymentDetailsScreen.kt
@@ -11,22 +11,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import com.google.accompanist.swiperefresh.SwipeRefresh
-import com.google.accompanist.swiperefresh.SwipeRefreshState
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.mateusz113.financemanager.R
 import com.mateusz113.financemanager.presentation.common.components.TopAppBarWithBack
 import com.mateusz113.financemanager.presentation.common.dialog.PhotoDisplayDialog
@@ -34,6 +37,7 @@ import com.mateusz113.financemanager.presentation.common.wrapper.ScaffoldWrapper
 import com.mateusz113.financemanager.presentation.destinations.PaymentAdditionScreenDestination
 import com.mateusz113.financemanager.presentation.payments.payment_details.components.PaymentDetailsInfoBlock
 import com.mateusz113.financemanager.presentation.payments.payment_details.components.PaymentDetailsPhotosRow
+import com.mateusz113.financemanager.util.TestTags
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -48,13 +52,9 @@ fun PaymentDetailsScreen(
     navController: NavController
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
-    val swipeRefreshState = rememberSwipeRefreshState(
-        isRefreshing = state.isLoading
-    )
 
     PaymentDetailsScreenContent(
         state = state,
-        swipeRefreshState = swipeRefreshState,
         navController = navController,
         onRefresh = {
             viewModel.onEvent(PaymentDetailsEvent.Refresh)
@@ -78,10 +78,10 @@ fun PaymentDetailsScreen(
 }
 
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PaymentDetailsScreenContent(
     state: PaymentDetailsState<*>,
-    swipeRefreshState: SwipeRefreshState = SwipeRefreshState(isRefreshing = false),
     navController: NavController = NavController(LocalContext.current),
     onRefresh: () -> Unit,
     onPhotoClick: (String) -> Unit,
@@ -89,6 +89,13 @@ fun PaymentDetailsScreenContent(
     onPhotoDialogDismiss: () -> Unit
 ) {
     if (state.error == null) {
+        val pullRefreshState =
+            rememberPullRefreshState(
+                refreshing = state.isLoading,
+                onRefresh = onRefresh,
+                refreshThreshold = 60.dp
+            )
+
         ScaffoldWrapper(
             topAppBar = {
                 TopAppBarWithBack(
@@ -97,15 +104,22 @@ fun PaymentDetailsScreenContent(
                 )
             }
         ) {
-            SwipeRefresh(
-                state = swipeRefreshState,
-                onRefresh = onRefresh
+            Box(
+                modifier = Modifier.pullRefresh(pullRefreshState)
             ) {
+                PullRefreshIndicator(
+                    refreshing = state.isLoading,
+                    state = pullRefreshState,
+                    modifier = Modifier
+                        .testTag(TestTags.SWIPE_REFRESH_INDICATOR)
+                        .align(Alignment.TopCenter)
+                )
                 state.paymentDetails?.let { details ->
                     Column(
                         modifier = Modifier
                             .padding(it)
-                            .verticalScroll(rememberScrollState()),
+                            .testTag(TestTags.SWIPE_REFRESH)
+                            .verticalScroll(rememberScrollState())
                     ) {
                         PaymentDetailsInfoBlock(
                             modifier = Modifier.padding(horizontal = 10.dp),
