493a1505bb569f0774fe43804a2b6e0ea61e1044
Mateusz113 <mateusz.ch.113@gmail.com>
2024-03-23 16:48:12 +0100

    Update SpendingDetailsScreen.kt
    -Replaced SwipeRefresh with PullRefresh
    -Added test tag for column holding the scrollable content

--- app/src/main/java/com/mateusz113/financemanager/presentation/spendings/spending_details/SpendingDetailsScreen.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/spendings/spending_details/SpendingDetailsScreen.kt
@@ -1,23 +1,35 @@
 package com.mateusz113.financemanager.presentation.spendings.spending_details
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import co.yml.charts.ui.piechart.models.PieChartData
@@ -34,8 +46,10 @@ import com.mateusz113.financemanager.presentation.common.dialog.PaymentListingsC
 import com.mateusz113.financemanager.presentation.common.wrapper.ScaffoldWrapper
 import com.mateusz113.financemanager.presentation.destinations.PaymentDetailsScreenDestination
 import com.mateusz113.financemanager.presentation.spendings.spending_details.components.PaymentsChart
+import com.mateusz113.financemanager.util.TestTags
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.time.LocalDate
 
 @Destination
 @Composable
@@ -44,13 +58,9 @@ fun SpendingDetailsScreen(
     navigator: DestinationsNavigator
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
-    val swipeRefreshState = rememberSwipeRefreshState(
-        isRefreshing = state.isLoading
-    )
 
     SpendingDetailsScreenContent(
         state = state,
-        swipeRefreshState = swipeRefreshState,
         onRefresh = {
             viewModel.onEvent(SpendingDetailsEvent.Refresh)
         },
@@ -89,10 +99,10 @@ fun SpendingDetailsScreen(
     )
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SpendingDetailsScreenContent(
     state: SpendingDetailsState,
-    swipeRefreshState: SwipeRefreshState = SwipeRefreshState(false),
     onRefresh: () -> Unit,
     onFilterDialogOpen: () -> Unit,
     onSearchValueChange: (String) -> Unit,
@@ -103,16 +113,33 @@ fun SpendingDetailsScreenContent(
     onPaymentClick: (PaymentListing) -> Unit
 ) {
     ScaffoldWrapper { paddingValues ->
-        SwipeRefresh(
-            state = swipeRefreshState,
+        val searchBarHeight = 80.dp
+        val pullRefreshState = rememberPullRefreshState(
+            refreshing = state.isLoading,
             onRefresh = onRefresh,
-            modifier = Modifier.padding(paddingValues)
+            refreshThreshold = 60.dp
+        )
+        Box(
+            modifier = Modifier
+                .padding(paddingValues)
+                .pullRefresh(pullRefreshState)
         ) {
+            PullRefreshIndicator(
+                refreshing = state.isLoading,
+                state = pullRefreshState,
+                modifier = Modifier
+                    .testTag(TestTags.SWIPE_REFRESH_INDICATOR)
+                    .align(Alignment.TopCenter)
+                    .offset(y = searchBarHeight)
+                    .zIndex(1f)
+            )
+
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
                     .verticalScroll(rememberScrollState())
+                    .testTag(TestTags.SCROLLABLE_COLUMN)
             ) {
                 PaymentSearchBar(
                     modifier = Modifier
