d21f500c83ecb4751b631089b55ee3f01bb04e63
Mateusz113 <mateusz.ch.113@gmail.com>
2024-03-30 18:02:22 +0100

    Update SettingsScreenTest.kt
    Improved dismissOpenedDialog_dialogIsClosed test to be less flaky

--- app/src/androidTest/java/com/mateusz113/financemanager/presentation/settings/SettingsScreenTest.kt
+++ app/src/androidTest/java/com/mateusz113/financemanager/presentation/settings/SettingsScreenTest.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollToNode
 import androidx.compose.ui.text.AnnotatedString
+import androidx.test.espresso.Espresso.pressBack
 import com.google.common.truth.Truth.assertThat
 import com.mateusz113.financemanager.MainActivity
 import com.mateusz113.financemanager.R
@@ -111,8 +112,11 @@ class SettingsScreenTest {
         state.value = state.value.copy(
             isDialogOpen = true
         )
+        pressBack()
+        composeRule.waitForIdle()
+        assertThat(state.value.isDialogOpen).isFalse()
         composeRule.onNode(SemanticsMatcher.expectValue(SemanticsProperties.IsDialog, Unit))
-            .assertIsDisplayed()
+            .assertIsNotDisplayed()
     }
 
     @Test
