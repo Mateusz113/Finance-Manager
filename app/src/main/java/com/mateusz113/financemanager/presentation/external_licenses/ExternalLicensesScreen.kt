f8e0dcf560080cfafdeccef15a4a61b117a22c79
Mateusz113 <mateusz.ch.113@gmail.com>
2024-04-15 15:00:41 +0200

    Update ExternalLicensesScreen.kt
    -Replaced Column with LazyColumn as it is more appropriate for many elements
    -Added the externalLicensesList usage to display all the licenses with appropriate header
    -Updated the onItemClick lambda return type and how it is processed to account for agreements that have many parts

--- app/src/main/java/com/mateusz113/financemanager/presentation/external_licenses/ExternalLicensesScreen.kt
+++ app/src/main/java/com/mateusz113/financemanager/presentation/external_licenses/ExternalLicensesScreen.kt
@@ -1,26 +1,27 @@
 package com.mateusz113.financemanager.presentation.external_licenses
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.mateusz113.financemanager.R
-import com.mateusz113.financemanager.presentation.common.components.ClickableItemRow
+import com.mateusz113.financemanager.domain.enumeration.LicenseType
 import com.mateusz113.financemanager.presentation.common.components.TopAppBarWithBack
 import com.mateusz113.financemanager.presentation.common.dialog.TextInfoDialog
 import com.mateusz113.financemanager.presentation.common.wrapper.ScaffoldWrapper
-import com.mateusz113.financemanager.domain.enumeration.ExternalLicense
+import com.mateusz113.financemanager.presentation.external_licenses.components.externalLicensesList
+import com.mateusz113.financemanager.util.TestTags
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 
@@ -38,9 +39,11 @@ fun ExternalLicensesScreen(
         state = state,
         topBarLabel = R.string.external_licenses_notice,
         navController = navController,
-        licensesList = ExternalLicense.values().toList(),
-        onItemClick = { licenseTextId ->
-            val licenseText = context.getString(licenseTextId)
+        onItemClick = { licenseTextIds ->
+            var licenseText = ""
+            licenseTextIds.forEach { id ->
+                licenseText += context.getString(id)
+            }
             viewModel.onEvent(ExternalLicensesEvent.LicenseDialogStateUpdate(true))
             viewModel.onEvent(ExternalLicensesEvent.LicenseDialogInfoUpdate(licenseText))
         },
@@ -55,8 +58,7 @@ fun ExternalLicensesScreenContent(
     state: ExternalLicensesState,
     @StringRes topBarLabel: Int,
     navController: NavController = NavController(LocalContext.current),
-    licensesList: List<ExternalLicense>,
-    onItemClick: (Int) -> Unit,
+    onItemClick: (List<Int>) -> Unit,
     onDialogDismiss: () -> Unit
 ) {
     ScaffoldWrapper(
@@ -67,21 +69,29 @@ fun ExternalLicensesScreenContent(
             )
         }
     ) { innerPadding ->
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .verticalScroll(rememberScrollState()),
+                .testTag(TestTags.LAZY_COLUMN),
+            contentPadding = PaddingValues(vertical = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            licensesList.forEach { externalLicense ->
-                ClickableItemRow(
-                    label = stringResource(id = externalLicense.label),
-                    onClick = {
-                        onItemClick(externalLicense.licenseText)
-                    }
-                )
-            }
+            externalLicensesList(
+                license = LicenseType.APACHE2,
+                externalLicensesMap = state.externalLicensesMap,
+                onLicenseClick = onItemClick
+            )
+            externalLicensesList(
+                license = LicenseType.META_LICENSE,
+                externalLicensesMap = state.externalLicensesMap,
+                onLicenseClick = onItemClick
+            )
+            externalLicensesList(
+                license = LicenseType.ECLIPSE_PUBLIC_LICENSE,
+                externalLicensesMap = state.externalLicensesMap,
+                onLicenseClick = onItemClick
+            )
         }
 
         TextInfoDialog(
